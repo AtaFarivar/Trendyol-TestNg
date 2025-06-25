5d988b7732a7691e7bb442d11cc4be17836cbbd7
AtaFarivar <87602544+AtaFarivar@users.noreply.github.com>
2025-06-25 12:10:14 +0300

    delete the info

--- src/test/java/testData/LoginDataProvider.java
+++ src/test/java/testData/LoginDataProvider.java
@@ -7,9 +7,9 @@ public class LoginDataProvider {
     @DataProvider(name = "loginScenarios")
     public Object[][] loginScenarios() {
         return new Object[][]{
-                {"ata_farivar@yahoo.com", "", "Lütfen şifrenizi giriniz.", "Empty Password", 1},
-                {"ata_farivar@yahoo.com", "wrongPass123456", "E-posta adresiniz ve/veya şifreniz hatalı.", "Wrong Password", 2},
-                {"ata_farivar@yahoo.com", "6118945@T@", null, "Success Login", 3}
+                {"", "", "Lütfen şifrenizi giriniz.", "Empty Password", 1},
+                {"", "wrongPass123456", "E-posta adresiniz ve/veya şifreniz hatalı.", "Wrong Password", 2},
+                {"", "", null, "Success Login", 3}
         };
     }
 }
